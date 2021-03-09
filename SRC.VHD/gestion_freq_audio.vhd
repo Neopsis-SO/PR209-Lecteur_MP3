d8ff8fc3277f037e3a982b70bb036e0901065891
Neopsis-SO <72496337+Neopsis-SO@users.noreply.github.com>
2021-03-09 17:15:15 +0100

    Update gestion_freq_audio.vhd

--- SRC.VHD/gestion_freq_audio.vhd
+++ SRC.VHD/gestion_freq_audio.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -32,12 +32,30 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity gestion_freq_audio is
---  Port ( );
+    Port (  clk             : in    std_logic;
+            reset           : in    std_logic;
+            cePWM           : out   std_logic  -- Frequence de 44100Hz
+            );
 end gestion_freq_audio;
 
 architecture Behavioral of gestion_freq_audio is
-
+    signal ceP_couter   : unsigned  (11 DOWNTO 0);  -- 2^12 = 4 096
+    
 begin
+    process(clk, reset)
+    begin
+        if (reset = '1') then
 
-
+            ceP_couter  <= (OTHERS=> '0');
+            cePWM       <= '0';
+        elsif (clk'event and clk = '1') then
+                if (ceP_couter = 2767) then   -- (2 768 - 1)
+                ceP_couter  <= (OTHERS=> '0');
+                cePWM       <= '1';
+            else
+                ceP_couter  <= ceP_couter + 1;
+                cePWM       <= '0';
+            end if;
+        end if;
+    end process;
 end Behavioral;
