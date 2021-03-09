20e7ef2f20a57c4811ccf312d0dc05f93fdf527c
Neopsis-SO <72496337+Neopsis-SO@users.noreply.github.com>
2021-03-09 17:12:10 +0100

    Update PWM.vhd

--- SRC.VHD/PWM.vhd
+++ SRC.VHD/PWM.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -32,12 +32,41 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity PWM is
---  Port ( );
+    Port (  clk     : in    std_logic;
+            reset   : in    std_logic;
+            ce      : in    std_logic;
+            idata_n : in    std_logic_vector(10 downto 0);
+            odata   : out   std_logic
+            );
 end PWM;
 
 architecture Behavioral of PWM is
-
+    signal val_data : unsigned(11 downto 0);
+    signal counter  : unsigned(11 downto 0);
+    
 begin
 
+    val_data <= unsigned(signed(idata_n) + to_signed(1024,12));
 
+    process(clk, reset)
+    begin
+        if (reset = '1') then
+            counter <= to_unsigned(0, 12);
+            odata   <= '0';
+        elsif (clk'event and clk = '1') then
+            if (ce = '1') then
+                if (counter = 2067) then
+                    odata   <= '0';
+                    counter <= (OTHERS => '0');
+                else
+                    if (counter < val_data) then
+                        odata   <= '1';
+                    else
+                        odata   <= '0';
+                    end if;
+                    counter <= counter + 1;
+                end if;
+            end if;
+        end if;
+    end process;
 end Behavioral;
