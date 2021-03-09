e12c96d4d23a83dcc850f333b3ab7b9e7499516d
Neopsis-SO <72496337+Neopsis-SO@users.noreply.github.com>
2021-03-09 17:14:00 +0100

    Create gestion_freq_audio.vhd

---
+++ SRC.VHD/gestion_freq_audio.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: ENSEIRB-MATMECA
+-- Engineer: Maxime Dabrowski
+-- 
+-- Create Date: 09.03.2021 17:13:24
+-- Design Name: 
+-- Module Name: gestion_freq_audio - Behavioral
+-- Project Name: PR209-Lecteur_MP3
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity gestion_freq_audio is
+--  Port ( );
+end gestion_freq_audio;
+
+architecture Behavioral of gestion_freq_audio is
+
+begin
+
+
+end Behavioral;
