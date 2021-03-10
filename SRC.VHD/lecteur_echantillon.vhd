a8799a8e5dadde43de24e0216e278cfd2700534c
Neopsis-SO <72496337+Neopsis-SO@users.noreply.github.com>
2021-03-10 14:27:32 +0100

    Create lecteur_echantillon.vhd

---
+++ SRC.VHD/lecteur_echantillon.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: ENSEIRB-MATMECA
+-- Engineer: Maxime Dabrowski
+-- 
+-- Create Date: 10.03.2021 14:26:49
+-- Design Name: 
+-- Module Name: lecteur_echantillon - Behavioral
+-- Project Name: PR209-Lecteur_MP3
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity lecteur_echantillon is
+--  Port ( );
+end lecteur_echantillon;
+
+architecture Behavioral of lecteur_echantillon is
+
+begin
+
+
+end Behavioral;
