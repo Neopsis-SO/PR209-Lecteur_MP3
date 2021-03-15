4c2e33fce05d52fea5ab0847156262c147b7311f
Neopsis-SO <72496337+Neopsis-SO@users.noreply.github.com>
2021-03-15 17:22:11 +0100

    Update wav_ram.vhd

--- SRC.VHD/wav_ram.vhd
+++ SRC.VHD/wav_ram.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -32,12 +32,30 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity wav_ram is
---  Port ( );
+    Port (  clk         : in    std_logic;
+            r_w         : in    std_logic;  --Ecriture a 1 / Lecture a 0
+            addr_in_w   : in    std_logic_vector(17 downto 0);  --Adresse de la data devant être ecrite dans la RAM
+            data_in_w   : in    std_logic_vector(15 downto 0);  --Data devant être ecrite dans la RAM a l'adresse ci-dessus
+            addr_in_r   : in    std_logic_vector(17 downto 0);  --Addresse de la data allant au modulateur
+            data_out_r  : out   std_logic_vector(10 downto 0)   --Data allant jusqu au modulateur a partir de l adresse ci-dessus
+            );
 end wav_ram;
 
 architecture Behavioral of wav_ram is
-
+    TYPE RAM IS ARRAY (0 TO 262143) OF std_logic_vector(10 downto 0);   --2^18-1 = 262 143 espace memoire de donnees sur 11 bits
+    signal Mem : RAM;
+    
 begin
-
+    memory : process(clk)
+    begin
+        if(clk'event and clk='0') then
+            if(r_w = '0') then --Ecriture de 2* le if pour ne pas desynchroniser les modules si lecture et ecriture au même CE
+                data_out_r <= Mem(to_integer(unsigned(addr_in_r)));
+            end if;
+            if(r_w = '1') then
+                Mem(to_integer(unsigned(addr_in_w))) <= data_in_w(10 downto 0);
+            end if;
+        end if;
+    end process;
 
 end Behavioral;
