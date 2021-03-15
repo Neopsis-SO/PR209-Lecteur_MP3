e499323f9be0925bae304f366b2ef7b41015136e
Neopsis-SO <72496337+Neopsis-SO@users.noreply.github.com>
2021-03-15 17:04:04 +0100

    Create cpt_18bits.vhd

---
+++ SRC.VHD/cpt_18bits.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: ENSEIRB-MATMECA
+-- Engineer: Maxime Dabrowski
+-- 
+-- Create Date: 15.03.2021 17:03:12
+-- Design Name: 
+-- Module Name: cpt_18bits - Behavioral
+-- Project Name: PR209-Lecteur_MP3
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity cpt_18bits is
+--  Port ( );
+end cpt_18bits;
+
+architecture Behavioral of cpt_18bits is
+
+begin
+
+
+end Behavioral;
