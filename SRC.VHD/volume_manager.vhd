aadccd02c9f3b25c382ae26184256b512cc8addf
Neopsis-SO <72496337+Neopsis-SO@users.noreply.github.com>
2021-03-17 15:50:33 +0100

    Create volume_manager.vhd

---
+++ SRC.VHD/volume_manager.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: ENSEIRB-MATMECA
+-- Engineer: Maxime Dabrowski
+-- 
+-- Create Date: 17.03.2021 15:49:02
+-- Design Name: 
+-- Module Name: volume_manager - Behavioral
+-- Project Name: PR209-Lecteur_MP3
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity volume_manager is
+--  Port ( );
+end volume_manager;
+
+architecture Behavioral of volume_manager is
+
+begin
+
+
+end Behavioral;
