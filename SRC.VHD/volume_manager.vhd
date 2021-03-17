1a00a4d84c7a33d50119771f5e33f792a428c839
Neopsis-SO <72496337+Neopsis-SO@users.noreply.github.com>
2021-03-17 16:16:19 +0100

    Update volume_manager.vhd

--- SRC.VHD/volume_manager.vhd
+++ SRC.VHD/volume_manager.vhd
@@ -32,12 +32,40 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity volume_manager is
---  Port ( );
+    Port (  clk     : in    std_logic;
+            reset   : in    std_logic;
+            ce      : in    std_logic;
+            switch  : in    std_logic_vector(3 downto 0);
+            idata   : in    std_logic_vector(10 downto 0);
+            odata   : out   std_logic_vector(10 downto 0)
+            );
 end volume_manager;
 
 architecture Behavioral of volume_manager is
 
 begin
 
+    compteur_adresse : process(clk, reset)
+    begin
+        if (reset = '1') then
+            odata  <= (OTHERS => '0');
+        elsif (clk'event and clk = '1') then
+            if (ce = '1') then
+                case switch is
+                    when "0000" => odata <= idata(10 downto 9)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10);
+                    when "0001" => odata <= idata(10 downto 8)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10);
+                    when "0010" => odata <= idata(10 downto 7)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10);
+                    when "0011" => odata <= idata(10 downto 6)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10);
+                    when "0100" => odata <= idata(10 downto 5)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10);
+                    when "0101" => odata <= idata(10 downto 4)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10);
+                    when "0110" => odata <= idata(10 downto 3)&idata(10)&idata(10)&idata(10)&idata(10);
+                    when "0111" => odata <= idata(10 downto 2)&idata(10)&idata(10)&idata(10);
+                    when "1000" => odata <= idata(10 downto 1)&idata(10)&idata(10);
+                    when "1001" => odata <= idata;
+                    when OTHERS => odata <= idata(10 downto 4)&idata(10)&idata(10)&idata(10)&idata(10)&idata(10);
+                end case;
+            end if;
+        end if;
+    end process;
 
 end Behavioral;
