845d5342d9867b32bd40044b5c4a42c50a81bf1d
Neopsis-SO <72496337+Neopsis-SO@users.noreply.github.com>
2021-03-24 10:38:31 +0100

    Create speed_manager.vhd

---
+++ SRC.VHD/speed_manager.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: ENSEIRB-MATMECA
+-- Engineer: Maxime Dabrowski
+-- 
+-- Create Date: 24.03.2021 10:37:45
+-- Design Name: 
+-- Module Name: speed_manager - Behavioral
+-- Project Name: PR209-Lecteur_MP3
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity speed_manager is
+--  Port ( );
+end speed_manager;
+
+architecture Behavioral of speed_manager is
+
+begin
+
+
+end Behavioral;
